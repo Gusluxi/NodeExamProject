b12c52f1376c68c658809220c118539bf98df358
Lobfohns <ludv1428@stud.kea.dk>
2022-05-31 23:36:21 +0200

    test

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+//hej
 app.listen(PORT, () => {
     console.log("Starting server on port:", PORT);
 });
