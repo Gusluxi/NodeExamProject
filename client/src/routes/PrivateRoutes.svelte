fb5a2fff9f0250fab8b027d2d35b5d13c3b0af29
Ludvig <ludvig.fohns@gmail.com>
2022-06-17 16:51:53 +0200

    Private routes initial commit

---
+++ client/src/routes/PrivateRoutes.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { Route } from "svelte-navigator";
+    import RouteGuard from "./RouteGuard.svelte";
+    export let path;
+  </script>
+
+  <Route {path} let:params let:location let:navigate>
+    <RouteGuard>
+      <slot {params} {location} {navigate} />
+    </RouteGuard>
+  </Route>
---
+++ client/src/routes/RouteGuard.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { useNavigate, useLocation } from "svelte-navigator";
+    import { user } from "../store/generalStore.js";
+    const navigate = useNavigate();
+    const location = useLocation();
+    $: if (!$user.loggedIn) {
+      navigate("/", {
+        state: { from: $location.pathname },
+        replace: true,
+      });
+    }
+  </script>
+
+  {#if $user}
+    <slot />
+  {/if}
