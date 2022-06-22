31a84aac4706e204aafdea69d2bbf0d72ba1042e
Lobfohns <ludv1428@stud.kea.dk>
2022-06-22 20:27:25 +0200

    started working on question component

---
+++ client/src/components/Question.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { baseURL } from "../../stores/generalStore.js";
+	let title;
+	let result = null
+	
+	async function newQuestion () {
+        const response = await fetch($baseURL + '/api/questions', {
+			method: 'POST',
+            headers: {
+                "content-type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+				question,
+				questiontype,
+				selectedSurveyId
+			})
+		});
+		
+		const json = await response.json()
+		result = JSON.stringify(json)
+	}
+</script>
+
+
+
+
--- client/src/pages/SurveyManager/EditSurvey.svelte
+++ client/src/pages/SurveyManager/EditSurvey.svelte
@@ -5,6 +5,6 @@
 
     const params = useParams()
     console.log($params);  
-
+    //Man skal kunne adde et question når man har lavet det. Man skal kunne ændre title på survey, evt ændre på question who knows.
 </script>
 <h1> Edit Survey</h1>
