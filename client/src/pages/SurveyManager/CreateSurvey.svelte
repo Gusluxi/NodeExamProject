c847e20e5e0742db4a8593b65e23f27a05747168
Lobfohns <ludv1428@stud.kea.dk>
2022-06-23 14:16:18 +0200

    create survey in svelte

--- client/src/pages/SurveyManager/CreateSurvey.svelte
+++ client/src/pages/SurveyManager/CreateSurvey.svelte
@@ -1,1 +1,22 @@
-<h1>Create Survey</h1>
+<script>
+    import { baseURL } from "../../stores/generalStore.js";
+	let title;
+	
+	async function newSurvey () {
+        const response = await fetch($baseURL + '/api/surveys', {
+			method: 'POST',
+            headers: {
+                "content-type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+				title
+			})
+		});
+		
+		const result = await response.json();
+        console.log(result);
+	}
+</script>
+<input bind:value={title}>
+<button class="btn" on:click="{newSurvey}">New Survey</button>
